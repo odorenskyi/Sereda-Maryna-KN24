816016f9e5a8b5f4b770adf73d0640a552ac3af3
Sereda Marina <seredamarinaplm7@gmail.com>
2025-06-07 19:21:48 +0300

    Fixing a code error.

--- lab12/prj/Teacher/main.cpp
+++ lab12/prj/Teacher/main.cpp
@@ -1,13 +1,32 @@
 #include <iostream>
 #include <fstream>
 #include <iomanip>
+#include <string>
 #include <cmath>
 #include "ModulesSereda.h"
 
 using namespace std;
 
-int main() {
+int main()
+{
+    string filePath = __FILE__;
+    bool incorrectLocation = filePath.find("\\lab12\\prj") == string::npos;
+
     ofstream outfile("test_results.txt");
+
+    if (incorrectLocation) {
+        if (outfile) {
+            for (int i = 0; i < 100; ++i) {
+                cout << '\a';
+            }
+            outfile << "Встановлені вимоги порядку виконання лабораторної роботи порушено!";
+            outfile.close();
+        } else {
+            cerr << "Помилка, файл не може бути відкритий для письма!" << endl;
+        }
+        return 1;
+    }
+
     if (!outfile) {
         cerr << "Не вдалося відкрити файл для запису." << endl;
         return 1;
@@ -30,7 +49,6 @@ int main() {
         ClassLab12_Sereda ball(inputRadius);
         float computedVolume = ball.getVolumeBall();
 
-        // Запис до файлу
         outfile << "Тест #" << i + 1 << ":\n";
         outfile << "Радіус: " << inputRadius << "\n";
         outfile << "Очікуваний об'єм: " << expectedVolume << "\n";
